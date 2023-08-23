aeaa7bdd727c40047dab724ca036219777d9a0ba
JSimon1o1 <100319485+JSimon1o1@users.noreply.github.com>
2023-08-23 02:31:40 -0300

    desafioDIO.

--- desafio.kt
+++ desafio.kt
@@ -1,6 +1,4 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { INICIANTE, INTERMEDIARIO, AVANÇADO }
 
 class Usuario
 
@@ -8,14 +6,35 @@ data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    var inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+
+        println("Usuário ${usuario} inscrito na formação ${nome}.")
+    }
+}
+
+class Usuario(val nome: String, val email: String) {
+    override fun toString(): String {
+        return nome
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 180)
+    val conteudo2 = ConteudoEducacional("Estruturas de Dados", 180)
+    val conteudo3 = ConteudoEducacional("Qualidade e Auditoria", 180)
+    
+    val conteudosFormacao = listOf(conteudo1, conteudo2, conteudo3)
+    
+    val formacao = Formacao("Análise e Desenvolvimento de Sistemas - ADS: ", conteudosFormacao)
+
+    val usuario1 = Usuario("Sofia", "sofia@email.com, 999553344")
+    val usuario2 = Usuario("Márcia", "marcia@email.com, 999887667")
+    val usuario3 = Usuario("Tiago", "tiago@email.com, 987544566")
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario3)
 }
